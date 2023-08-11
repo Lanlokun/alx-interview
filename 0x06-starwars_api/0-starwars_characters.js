e90690afa49f2e872d377752a5111c5cdbd0d4f6
malik <mlanlokun@gmail.com>
2023-08-11 19:26:49 +0300

    updates

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-// Write a script that prints all characters of a Star Wars movie:
 
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
