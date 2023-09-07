084943639b90cb32cdb46a8a291a562211a8f54b
lanlokun <mlanlokun@gmail.com>
2023-09-07 19:13:06 +0300

    Prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,23 +2,57 @@
 """Prime Game"""
 
 
+
 def isWinner(x, nums):
-    """Returns: name of the player that won the most rounds"""
-    if not nums or x < 1:
-        return None
-    n = max(nums)
-    sieve = [True for _ in range(n + 1)]
-    sieve[0] = False
-    sieve[1] = False
-    for i in range(2, int(n ** 0.5) + 1):
-        if sieve[i] is True:
-            for j in range(i * i, n + 1, i):
-                sieve[j] = False
-    sieve = [i for i in range(n + 1) if sieve[i] is True]
-    c = 0
-    for n in nums:
-        if n in sieve:
-            c += 1
-    if c % 2 == 0:
+    """Function to get who has won in prime game"""
+    mariaWinsCount = 0
+    benWinsCount = 0
+
+    for num in nums:
+        roundsSet = list(range(1, num + 1))
+        primesSet = primes_in_range(1, num)
+
+        if not primesSet:
+            benWinsCount += 1
+            continue
+
+        isMariaTurns = True
+
+        while(True):
+            if not primesSet:
+                if isMariaTurns:
+                    benWinsCount += 1
+                else:
+                    mariaWinsCount += 1
+                break
+
+            smallestPrime = primesSet.pop(0)
+            roundsSet.remove(smallestPrime)
+
+            roundsSet = [x for x in roundsSet if x % smallestPrime != 0]
+
+            isMariaTurns = not isMariaTurns
+
+    if mariaWinsCount > benWinsCount:
         return "Maria"
-    return "Ben"
+
+    if mariaWinsCount < benWinsCount:
+        return "Ben"
+
+    return None
+
+
+def is_prime(n):
+    """Returns True if n is prime, else False."""
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+def primes_in_range(start, end):
+    """Returns a list of prime numbers between start and end (inclusive)."""
+    primes = [n for n in range(start, end+1) if is_prime(n)]
+    return primes
